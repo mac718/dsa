590033540a700d5ac530144b4dfc0ad69b18450b
Mike Coon <mac718@gmail.com>
2023-07-21 12:02:22 -0700

    add diameter of binary tree (leetcode) python solution

--- LeetCode/diameter_of_binary_tree.py
+++ LeetCode/diameter_of_binary_tree.py
@@ -2,12 +2,17 @@ from typing import Optional
 
 
 def diameterOfBinaryTree(root: Optional[TreeNode]) -> int:
+    res = 0
+
     def dfs(node):
+        nonlocal res
         if node is None:
             return 0
-        return max(1 + dfs(node.left), 1 + dfs(node.right))
-    left = dfs(root.left)
-    right = dfs(root.right)
 
-    return right + left
+        left = dfs(node.left)
+        right = dfs(node.right)
+        res = max(res, left + right)
+        return max(1 + left, 1 + right)
 
+    dfs(root)
+    return res
